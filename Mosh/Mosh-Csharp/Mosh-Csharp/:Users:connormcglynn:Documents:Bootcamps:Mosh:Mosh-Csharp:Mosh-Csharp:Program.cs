fe7d2bfb6f40d7755c10a3db5c7fc908041b8628
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-08-20 11:32:32 -0400

    fixed directory

--- Mosh/Mosh-Csharp/Mosh-Csharp/:Users:connormcglynn:Documents:Bootcamps:Mosh:Mosh-Csharp:Mosh-Csharp:Program.cs
+++ Mosh/Mosh-Csharp/Mosh-Csharp/:Users:connormcglynn:Documents:Bootcamps:Mosh:Mosh-Csharp:Mosh-Csharp:Program.cs
@@ -4,9 +4,10 @@ namespace MoshCsharp
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte number = 2;
+            Console.WriteLine(number);
         }
     }
 }
