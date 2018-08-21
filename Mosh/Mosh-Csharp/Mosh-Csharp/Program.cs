7966c1ecb209b0f46ae16a3d9f285d0799683af1
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-08-21 11:58:10 -0400

    Lists<>

--- Mosh/Mosh-Csharp/Mosh-Csharp/Program.cs
+++ Mosh/Mosh-Csharp/Mosh-Csharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MoshCsharp
 {
@@ -14,53 +15,30 @@ namespace MoshCsharp
     {
         static void Main(string[] args) 
         {
-            var random = new Random();
-            const int pwLength = 10;
+            var numbers = new List<int>() {1, 2, 3, 4};
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
 
-            var buffer = new char[pwLength];
-            for (var i = 0; i < pwLength; i++)
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+            Console.WriteLine("Last index of 1: " + numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: " + numbers.Count);
+
+            for (var i = 0; i < numbers.Count; i++)
             {
-                buffer[i] = (char)('a' + random.Next(0, 26));
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
             }
 
-            var password = new string(buffer);
-            Console.WriteLine(password);
+            foreach (var number in numbers)
+                Console.WriteLine(number);
 
-            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+            numbers.Clear();
+            Console.WriteLine(numbers.Count);
 
-            // Length
-            Console.WriteLine("Length: " + numbers.Length);
-
-            // IndexOf()
-            var index = Array.IndexOf(numbers, 9);
-            Console.WriteLine("Index of 9:" + index);
-
-            // Clear() set array item to 0
-            Array.Clear(numbers, 0, 2);
-
-            Console.WriteLine("Effect of Clear()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-
-            // Copy()
-            var another = new int[3];
-            Array.Copy(numbers, another, 3);
-
-            Console.WriteLine("Effect of Copy()");
-            foreach (var n in another)
-                Console.WriteLine(n);
-            
-            // Sort() 
-            Array.Sort(numbers);
-            Console.WriteLine("Effect of Sort()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-
-            // Reverse()
-            Array.Reverse(numbers);
-            Console.WriteLine("Effect of Reverse()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
 
         }
     }
