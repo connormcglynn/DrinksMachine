c8bb0305ae3ec07407f3b81c26adcdb37f75dc44
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-08-21 10:47:07 -0400

    for / foreach / while

--- Mosh/Mosh-Csharp/Mosh-Csharp/Program.cs
+++ Mosh/Mosh-Csharp/Mosh-Csharp/Program.cs
@@ -14,40 +14,18 @@ namespace MoshCsharp
     {
         static void Main(string[] args)
         {
-            //bool isGoldCustomer = true;
-
-            ////float price;
-            ////if (isGoldCustomer)
-            ////{
-            ////    price = 19.95f;
-            ////}
-            ////else
-            ////{
-            ////    price = 29.95f;
-            ////}
-
-            //float price = (isGoldCustomer) ? 19.95f : 29.95f;
-
-            // CASE
-
-            var season = Season.Autumn;
-
-            switch (season)
+            while (true)
             {
-                case Season.Autumn:
-                    Console.WriteLine("It's autumn and a beautiful season.");
-                    break;
+                Console.Write("Type your name:");
+                var input = Console.ReadLine();
 
-                case Season.Winter:
-                    Console.WriteLine("It's summer, time for the beach!");
-                    break;
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                }
 
-                default:
-                    Console.WriteLine("I have no clue what season it is!");
-                    break;
-                    
+                break;
             }
-
         }
     }
 }
