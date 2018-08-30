71798a982cafc469224acf0f5720c139215f609d
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-08-30 11:25:52 -0400

    update

---
+++ Mosh/Mosh-Csharp/Mosh-Csharp/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace MoshCsharp
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+    }
+
+
+}
--- Mosh/Mosh-Csharp/Mosh-Csharp/Program.cs
+++ Mosh/Mosh-Csharp/Mosh-Csharp/Program.cs
@@ -3,11 +3,17 @@ using System.Text;
 
 namespace MoshCsharp
 {
+
     class Program
     {
         static void Main(string[] args) 
         {
-            
+            var customer = new Customer(1, "John");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
         }
     }
+
+
 }
